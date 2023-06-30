12a670c29f3afbcd8b98b7767e523de60e087148
Terje Christensen <terchris@users.noreply.github.com>
2023-06-30 11:35:55 +0200

    Update init.sql
    new init script that display status

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,3 +1,14 @@
-CREATE DATABASE urbalurba_db;
-CREATE USER strapi WITH ENCRYPTED PASSWORD 'Smartbygutta01';
-GRANT ALL PRIVILEGES ON DATABASE urbalurba_db TO strapi;
+DO $$ 
+BEGIN
+    -- Try to create the database
+    CREATE DATABASE urbalurba_db;
+    RAISE NOTICE 'Database created successfully';
+    
+    -- Try to create the user
+    CREATE USER strapi WITH ENCRYPTED PASSWORD 'Smartbygutta01';
+    RAISE NOTICE 'User created successfully';
+EXCEPTION
+    -- If there is an error, display a message
+    WHEN others THEN
+        RAISE NOTICE 'Error: %', SQLERRM;
+END $$;
